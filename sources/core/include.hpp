4a988d14833908eceb1ea06689073e83027e9fd4
fuchsuaproject <fuchsuaproject@hotmail.com>
2025-10-22 12:56:55 +0800

    어쩌라고 git아~

--- sources/core/include.hpp
+++
@@ -1,1 +0,0 @@
-#include "include/defines.hpp"
--- sources/core/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include<include.cpp>
-
-unsigned long start(void);
-
-unsigned long GRUB_Header_Array[0x30]__attribute__((section(".multiboot"))) = {0x1badb002, 0x00007, -(0x1badb002 + 0x00007),
-    (unsigned long)GRUB_Header_Array, 0jx100000, 0, 0, (unsigned long)&start};
